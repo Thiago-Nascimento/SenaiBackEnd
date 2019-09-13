f45180c461b2ea3d373f76340a17c23eb47a350c
FIC <FIC@info.local>
2019-09-13 11:12:04 -0300

    Update

--- aula1/Program.cs
+++ aula1/Program.cs
@@ -12,15 +12,37 @@ namespace aula1
             // Declaração e atribuição de valor
             int numero2 = 5;
 
-            // Declaração de varias variaveis e atribuição de valor
-            int numero3, numero4 = 7;
+            // Declaração de varias variaveis
+            int numero3, numero4, numero5;
 
             float media;
 
+            // Tipo booleano
             bool resultado = false;
 
+            string nomeAluno;
 
-            Console.WriteLine(numero2 + numero4);
+            Console.WriteLine("Bem Vindos a nossa primeira aplicação!");
+
+            Console.Write("Digite seu nome: ");
+            nomeAluno = Console.ReadLine();
+
+            Console.WriteLine(nomeAluno + ", agora digite sua primeira nota: ");
+            // numero3 = Convert.ToInt32(Console.ReadLine());
+            numero3 = int.Parse(Console.ReadLine());
+
+            // Interpolação
+            Console.WriteLine($"Sua primeira nota é {numero3}");
+
+            Console.Write("Digite sua segunda nota: ");
+            numero4 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite sua terceira nota: ");
+            numero5 = int.Parse(Console.ReadLine());
+
+            media = (numero3 + numero4 + numero5) / 3;
+
+            Console.WriteLine($"Sua média é {media}");
         }
     }
 }
