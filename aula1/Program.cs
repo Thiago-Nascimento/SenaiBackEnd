cf6287defd2fc958d95f31a842aa6ce43445a803
FIC <FIC@info.local>
2019-09-13 09:36:44 -0300

    First Commit

---
+++ aula1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace aula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Somente declaração
+            int numero1;
+
+            // Declaração e atribuição de valor
+            int numero2 = 5;
+
+            // Declaração de varias variaveis e atribuição de valor
+            int numero3, numero4 = 7;
+
+            float media;
+
+            bool resultado = false;
+
+
+            Console.WriteLine(numero2 + numero4);
+        }
+    }
+}
