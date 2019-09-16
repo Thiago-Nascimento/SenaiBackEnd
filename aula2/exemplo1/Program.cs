0dd912ebf21e2b287832362b016d2c71806d491a
FIC <FIC@info.local>
2019-09-16 11:48:12 -0300

    Exemplos da Aula 2 adicionados

---
+++ aula2/exemplo1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace exemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, n3, media;
+
+            Console.Write("Digite a primeira nota: ");
+            n1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a segunda nota: ");
+            n2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a primeira nota: ");
+            n3 = double.Parse(Console.ReadLine());
+
+            media = (n1 + n2 + n3) / 3;
+
+            if(media >= 6) {
+                Console.WriteLine("Aprovado");
+            } else {
+                Console.WriteLine("Reprovado");
+            }
+        }
+    }
+}
---
+++ aula2/exemplo2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exemplo2 {
+    class Program {
+        static void Main (string[] args) {
+            double n1, n2, n3, media;
+
+            Console.Write ("Digite a primeira nota: ");
+            n1 = double.Parse (Console.ReadLine ());
+
+            Console.Write ("Digite a segunda nota: ");
+            n2 = double.Parse (Console.ReadLine ());
+
+            Console.Write ("Digite a primeira nota: ");
+            n3 = double.Parse (Console.ReadLine ());
+
+            media = (n1 + n2 + n3) / 3;
+
+            if (media >= 6) {
+                Console.WriteLine ("Aprovado");
+            } else {
+                if (media >= 4) {
+                    Console.WriteLine ("Recuperação");
+                } else {
+                    Console.WriteLine ("Reprovado");
+                }
+            }
+        }
+    }
+}
