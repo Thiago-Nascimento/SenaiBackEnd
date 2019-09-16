cccf25f66528999f4afce82cb154a7b0bc57baeb
Thiago-Nascimento <thiago.r.nascimento@hotmail.com>
2019-09-16 14:14:18 -0300

    Exemplo 3 concluído

---
+++ aula2/exemplo3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace exemplo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            bool entrada = false;
+            string resposta;
+
+            Console.Write("Digite sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            if(idade > 17) {
+                entrada = true;
+            }
+
+            if(entrada == true) {
+                resposta = "Entrada Permitida";
+            } else {
+                resposta = "Entrada Negada";
+            }
+
+            Console.WriteLine(resposta);
+        }
+    }
+}
